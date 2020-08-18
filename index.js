f7fbf9b06cf950cfafc5b235476cc2ed630e0cd2
matthewlazewski <matthew.lazewski@gmail.com>
2020-08-18 13:27:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(sourceArray, fn){
+    const arr = []
+    for (let i = 0; i < sourceArray.length; i++){
+        arr.push(fn(sourceArray[i]))
+    }
+    return arr
+}
+
+function reduce(sourceArray, fn, start){
+    let reduced;
+    let i = 0;
+    
+    if (start){
+        reduced = start 
+    } else {
+        reduced = sourceArray[0]
+        i ++;
+    }
+    for (i; i < sourceArray.length; i++){
+        reduced = fn(sourceArray[i], reduced)
+    }
+    return reduced 
+};
+
